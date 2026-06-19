6010b8431a6406fb070aae2495b30fe8cdca5f14
Balazs.R <balazsrehm89@gmail.com>
2026-06-19 11:12:57 +0200

    Update LoginResponse.java

--- backend/src/main/java/com/cortis/auth/dto/LoginResponse.java
+++ backend/src/main/java/com/cortis/auth/dto/LoginResponse.java
@@ -1,17 +1,8 @@
 package com.cortis.auth.dto;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Setter
-public class LoginResponse {
-
-    private String username;
-    private boolean authenticated;
+public record LoginResponse(
+        String username,
+        boolean authenticated,
+        String token
+) {
 }
