3a7200ada90b9aea01dc5a2f159bbe58a84bfa43
Balazs8998 <balazsrehm89@gmail.com>
2026-06-22 22:50:02 +0200

    refactor(project): restructure modules and configure Flyway, logging and environment variables

--- app/backend/src/main/java/com/cortis/core/session/JwtService.java
+++ app/backend/src/main/java/com/cortis/core/session/JwtService.java
@@ -2,26 +2,32 @@ package com.cortis.core.session;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.util.Date;
 
 @Service
 public class JwtService {
 
-    private static final String SECRET ="1f5A7afeh6g24562g3d2fD4G2D5Se22nM";
+    private final String secretKey;
+    private final long expirationSeconds;
 
-    private static final long EXPIRATION_SECONDS = 60 * 60 * 8;
-
+    public JwtService(
+            @Value("${jwt.secret-key}") String secretKey,
+            @Value("${jwt.expiration-seconds}") long expirationSeconds
+    ) {
+        this.secretKey = secretKey;
+        this.expirationSeconds = expirationSeconds;
+    }
     private SecretKey getSigningKey() {
-        return Keys.hmacShaKeyFor(
-                SECRET.getBytes(StandardCharsets.UTF_8)
-        );
+         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+
+         return Keys.hmacShaKeyFor(keyBytes);
     }
 
     public String generateToken(UserDetails userDetails) {
@@ -30,7 +36,7 @@ public class JwtService {
         return Jwts.builder()
                 .subject(userDetails.getUsername())
                 .issuedAt(Date.from(now))
-                .expiration(Date.from(now.plusSeconds(EXPIRATION_SECONDS)))
+                .expiration(Date.from(now.plusSeconds(expirationSeconds)))
                 .signWith(getSigningKey())
                 .compact();
     }
